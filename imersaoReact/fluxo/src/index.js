cf75218fadd681ffa1bf19025832e580fd36644a
Floydfish <38674510+Floydfish@users.noreply.github.com>
2020-07-29 00:10:00 +0200

    Add react-router-dom

--- imersaoReact/fluxo/src/index.js
+++ imersaoReact/fluxo/src/index.js
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+function CadastroVideo() {
+  return (
+    <div>
+      Página de Cadastro de Video
+    </div>
+  )
+}
+
+const Pagina404 = () => (<div>Página Não Encontrada - Erro 404 </div>)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/" component={App} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
