dda70b90540af71388aeedd81c091e626670677f
Floydfish <38674510+Floydfish@users.noreply.github.com>
2020-07-31 11:27:42 +0200

    Adding propTypes

--- imersaoReact/fluxo/src/components/FormField/index.js
+++ imersaoReact/fluxo/src/components/FormField/index.js
@@ -1,29 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function FormField({ label, type, value, name, onChange }) {
+function FormField({
+  label, type, value, name, onChange,
+}) {
+  const fieldId = `id_${name}`;
+
   return (
     <div>
-      <label>
+      <label
+        htmlFor={fieldId}
+      >
         {label}
-        {type !== 'textarea' && 
+        {type !== 'textarea'
+          && (
           <input
+            id={fieldId}
             type={type}
             value={value}
             name={name}
             onChange={onChange}
           />
-        }
-        {type === 'textarea' && 
+          )}
+        {type === 'textarea'
+          && (
           <textarea
             value={value}
             name={name}
             onChange={onChange}
           />
-        }
+          )}
       </label>
     </div>
-  )
+  );
 }
 
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+};
 
 export default FormField;
