a2002793378ccce1c6605f46457ff9ffbcf1dc75
Floydfish <38674510+Floydfish@users.noreply.github.com>
2020-08-09 20:18:34 +0200

    update server

---
+++ imersaoReact/fluxo/src/repositories/videos.js
@@ -0,0 +1,24 @@
+import config from '../config';
+
+const URL_VIDEOS = `${config.URL_BACKEND}/videos`;
+
+function create(objetoDoVideo) {
+  return fetch(`${URL_VIDEOS}?_embed=videos`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(objetoDoVideo),
+  })
+    .then(async (res) => {
+      if (res.ok) {
+        const answer = await res.json();
+        return answer;
+      }
+      throw new Error('Não foi possível cadastrar os dados');
+    });
+}
+
+export default {
+  create,
+};
